577f0cc5064053d8b6835a32615dba6528675938
Brad Anderson <banderso@nerdery.com>
2014-04-02 19:34:08 -0500

    Adding SETTINGS file for Heroku

---
+++ SETTINGS.js
@@ -0,0 +1,19 @@
+var SETTINGS = {
+
+    OANDA_API_TOKEN: '84e920a42618757697162a9a051e5bcd-5a90c663914bf3839c7702a69579fa09',
+
+    OANDA_ACCOUNT_ID: '1351425',
+
+    IS_TESTING: true,
+
+    MYSQL: {
+        host: 'localhost',
+        port: 8889,
+        user: 'root',
+        password: 'root',
+        database: 'automated-trader'
+    }
+
+};
+
+module.exports = SETTINGS;
